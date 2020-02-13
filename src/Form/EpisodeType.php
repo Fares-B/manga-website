0e67855d788e4fda9bb69a130ca92b40339802c7
Fares-B <fares.bourih@gmail.com>
2020-02-13 23:33:39 +0100

    update form episode

--- src/Form/EpisodeType.php
+++ src/Form/EpisodeType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Episode;
 use App\Entity\Anime;
+use App\Entity\Format;
+use App\Entity\Voice;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -15,10 +17,16 @@ class EpisodeType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('voice')
+            ->add('voice', EntityType::class, [
+                'class' => Voice::class,
+                'choice_label' => 'name'
+            ])
             ->add('season')
             ->add('episode')
-            ->add('format')
+            ->add('format', EntityType::class, [
+                'class' => Format::class,
+                'choice_label' => 'name'
+            ])
             ->add('video')
             // ->add('createdAt')
             // ->add('slug')
