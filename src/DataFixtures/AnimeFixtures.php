b04c8f9182413e76de990789d96f110e33b8463a
Fares-B <fares.bourih@gmail.com>
2020-02-13 03:23:30 +0100

    add fixtures to generate fake data

--- src/DataFixtures/AnimeFixtures.php
+++ src/DataFixtures/AnimeFixtures.php
@@ -4,99 +4,121 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+
+// use Doctrine\ORM\EntityManager;
+
 use App\Entity\Anime;
 use App\Entity\Episode;
+use App\Entity\Kind;
+use App\Entity\Type;
+use App\Entity\Status;
 
 class AnimeFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $arr = [
-            [
-                'title' => 'One Piece',
-                'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam illo incidunt architecto similique itaque quis nisi officia explicabo libero! Molestiae?',
-                'published' => 1999,
-                'type' => 'Shonen',
-                'kind' => ['Aventure', 'Combat', 'Comédie'],
-                'status' => 'En cours',
-                'alternative_title' => 'ワンピース',
-                'author' => 'Eiichirō Oda',
-                'country' => 'Japon',
-                'image' => 'https://image.adkami.com/3.jpg',
-                'slug' => 'one-piece'
-            ],
-            [
-                'title' => 'Hunter x hunter',
-                'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt minima earum laudantium vero eligendi numquam nam, totam dolore quam et!',
-                'published' => 2011,
-                'type' => 'Shonen',
-                'kind' => ['Aventure', 'Combat', 'Drame', 'Fantastique'],
-                'status' => 'Fin',
-                'alternative_title' => 'Hunter hunter',
-                'author' => 'Yoshihiro Togashi',
-                'country' => 'Japon',
-                'image' => 'https://image.adkami.com/34.jpg?1510262416',
-                'slug' => 'hunter-x-hunter'
-            ]
-        ];
 
-        foreach ($arr as $value) {
-            $anime = new Anime();
-            $anime->setTitle($value['title'])
-                ->setContent($value['content'])
-                ->setPublished($value['published'])
-                ->setType($value['type'])
-                ->setKind($value['kind'])
-                ->setStatus($value['status'])
-                ->setAlternativeTitle($value['alternative_title'])
-                ->setAuthor($value['author'])
-                ->setCountry($value['country'])
-                ->setImage($value['image'])
-                ->setSlug($value['slug'])
-                ->setCreatedAt(new \DateTime());
+        $faker = \Faker\Factory::create('fr_FR');
 
-            $manager->persist($anime);
+        
+
+        // Kind
+        $kin = ["Action","Aventure","Combat","Comédie","Drama","Ecchi","École","Enfant","Fantastique","Harem","Historique","Horreur","Josei","Magique","Mecha","Militaire","Musique","Mystère","Policier","Psychologique","Romance","Science-fiction","Sport","Super Pouvoir","Thriller","Tranche de vie"];
+
+        foreach ($kin as $value) {
+            $kind = new Kind();
+
+            $kind->setName($value)
+                 ->setCreatedAt(new \DateTime());
+
+            $manager->persist($kind);
         }
 
-        $epi = [
-            [
-                'voice' => 'vostfr',
-                'season'=> 1,
-                'episode'=> 1,
-                'format'=> 'Episode',
-                'video' => ['1_url', '2_url'],
-                'slug' => 'one-piece-saison-1-episode-1-vostfr'
-            ],
-            [
-                'voice' => 'vostfr',
-                'season'=> 2,
-                'episode'=> 8,
-                'format'=> 'Episode',
-                'video' => ['1_url', '2_url'],
-                'slug' => 'naruto-shippuden-saison-2-episode-8-vostfr'
-            ],
-            [
-                'voice' => 'vf',
-                'season'=> 1,
-                'episode'=> 3,
-                'format'=> 'Film',
-                'video' => ['1_url', '2_url', '3_url', '4_url'],
-                'slug' => 'jojo-saison-1-film-3-vf'
-            ]
-        ];
-        foreach ($epi as $value) {
-            $episode = new Episode();
+        // Type
+        $typ = ["Shonen","Shojo","Seinen","Kodomo"];
+        foreach ($typ as $value) {
+            $type = new Type();
 
-            $episode->setVoice($value['voice'])
-                    ->setSeason($value['season'])
-                    ->setEpisode($value['episode'])
-                    ->setFormat($value['format'])
-                    ->setVideo($value['video'])
-                    ->setSlug($value['slug'])
-                    ->setCreatedAt(new \DateTime());
+            $type->setName($value)
+                 ->setCreatedAt(new \DateTime());
 
-            $manager->persist($episode);
+            $manager->persist($type);
         }
+
+        // Type
+        $sta = ["En cours","Fini","Abandonné"];
+        foreach ($sta as $value) {
+            $status = new Status();
+
+            $status->setName($value)
+                 ->setCreatedAt(new \DateTime());
+
+            $manager->persist($status);
+        }
+        // for ($i=1; $i < mt_rand(3, 8); $i++) { 
+        //     $anime = new Anime();
+
+        //     $name = $faker->word();
+
+        //     $em = new EntityManager();
+        //     $entityManager = $em->getDoctrine()->getManager();
+        //     $repoType = $entityManager->getRepository("App\Entity\Type")->find(mt_rand(1, 4));
+        //     $repoStatus = $entityManager->getRepository("App\Entity\Status")->find(mt_rand(1, 3));
+
+        //     $anime->setTitle($name)
+        //           ->setContent($faker->text($maxNbChars = 200))
+        //           ->setPublished($faker->numberBetween(1990,2020))
+        //           ->setType($repoType)
+        //           ->setKind(['Aventure', 'Combat', 'Drame', 'Fantastique'])
+        //           ->setStatus($repoStatus)
+        //           ->setAlternativeTitle($faker->sentence())
+        //           ->setAuthor($faker->name())
+        //           ->setCountry($faker->country())
+        //           ->setImage($faker->imageUrl())
+        //           ->setSlug($name)
+        //           ->setCreatedAt($faker->dateTimeBetween('-30 years'));
+        //     // $manager->persisxt($anime);
+        // }
+
+        // $epi = [
+        //     [
+        //         'voice' => 'vostfr',
+        //         'season'=> 1,
+        //         'episode'=> 1,
+        //         'format'=> 'Episode',
+        //         'video' => ['1_url', '2_url'],
+        //         'slug' => 'one-piece-saison-1-episode-1-vostfr'
+        //     ],
+        //     [
+        //         'voice' => 'vostfr',
+        //         'season'=> 2,
+        //         'episode'=> 8,
+        //         'format'=> 'Episode',
+        //         'video' => ['1_url', '2_url'],
+        //         'slug' => 'naruto-shippuden-saison-2-episode-8-vostfr'
+        //     ],
+        //     [
+        //         'voice' => 'vf',
+        //         'season'=> 1,
+        //         'episode'=> 3,
+        //         'format'=> 'Film',
+        //         'video' => ['1_url', '2_url', '3_url', '4_url'],
+        //         'slug' => 'jojo-saison-1-film-3-vf'
+        //     ]
+        // ];
+        // foreach ($epi as $value) {
+        //     $episode = new Episode();
+
+        //     $episode->setVoice($value['voice'])
+        //             ->setSeason($value['season'])
+        //             ->setEpisode($value['episode'])
+        //             ->setFormat($value['format'])
+        //             ->setVideo($value['video'])
+        //             ->setSlug($value['slug'])
+        //             ->setCreatedAt(new \DateTime());
+
+        //     $manager->persist($episode);
+        // }
 
         $manager->flush();
     }
