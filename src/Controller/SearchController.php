43751119a9e4a43b8cbd24c5605bf69422f14194
Fares-B <fares.bourih@gmail.com>
2020-02-22 23:38:44 +0100

    SearchController is not useful at the moment

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -2,33 +2,18 @@
 
 namespace App\Controller;
 
-use App\Form\SearchAnimeType;
-
 use App\Repository\Anime\AnimeRepository;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SearchController extends AbstractController
 {
     /**
-     * Recherche un anime dans la bdd et renvoi
-     * Créer ici une api rest pour ensuite pouvoir l'utiliser avec js (ajax)
-     * 
-     * @Route("/search", name="anime_search")
+     * Ce controller sera utile quand j'utiliserai des requetes ajax sous javascript
+     * @return json
      */
     public function searchAnime(Request $request, AnimeRepository $AnimeRepository)
     {
-        $searchForm = $this->createForm(SearchAnimeType::class);
-        $animes = null;
-        $searchForm->handleRequest($request);
-        if ($searchForm->isSubmitted() && $searchForm->isValid()) {
-            $criteria = $searchForm->getData();
-            $animes = $AnimeRepository->likeAnime($criteria);
-        }
-        return $this->render("search/index.html.twig", [
-            'formSearch' => $searchForm->createView(),
-            'animes' => $animes,
-        ]);
+        return "json";
     }
 }
