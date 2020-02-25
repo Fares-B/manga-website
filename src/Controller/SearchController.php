5cd1611cef637cbda91ecae861a69f0c252e4142
Fares-B <fares.bourih@gmail.com>
2020-02-25 21:01:53 +0100

    change name of route api_search_anime_title

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -11,17 +11,37 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SearchController extends AbstractController
 {
+    private $animeRepo;
+
+    public function __construct(AnimeRepository $animeRepo) {
+        $this->animeRepo = $animeRepo;
+    }
     /**
      * Retourne Du JSON, utiliser avec des requetes ajax (barre de recherche du site)
      * methode => POST
-     * @Route("/api/search/anime", name="api_search_anime")
+     * @Route("/api/search/anime/title", name="api_search_anime_title")
      * @return Response
      */
-    public function searchAnime(Request $request, AnimeRepository $repo)
+    public function searchAnimeTitle(Request $request)
     {
         $title = $request->request->get('title');
-        $result = $repo->findAnimeTitle($title);
-        $jsonResult = new JsonResponse($result);
-        return $jsonResult;
+        $result = $this->animeRepo->findAnimeTitle($title);
+        return $this->getJsonResponse($result);
+    }
+
+    /**
+     * @Route("/api/search/anime/criteria", name="api_search_anime_criteria")
+     */
+    public function searchAnimeCriteria(Request $request)
+    {
+        $criteria = $request->query->all();
+        $result = $this->animeRepo->searchAnime($criteria);
+        dd($result);
+        // ne marche pas pour l'instant car le repo demande une entité avec des getter
+        return $this->getJsonResponse(['ok' => 1]);
+    }
+    private function getJsonResponse($data)
+    {
+        return (new JsonResponse($data));
     }
 }
