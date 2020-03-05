2788b64baf13846939d17c557d7196a1a934161d
Fares-B <fares.bourih@gmail.com>
2020-03-05 21:19:23 +0100

    development of the function delete a comment

--- src/Controller/Comment/CommentController.php
+++ src/Controller/Comment/CommentController.php
@@ -3,8 +3,11 @@
 namespace App\Controller\Comment;
 
 use App\Entity\Comment\Comment;
+// use App\Form\Comment\CommentType;
+// use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * API Comment
@@ -17,20 +20,48 @@ class CommentController extends AbstractController
      * @Route("/add", name="add_comment")
      * @Route("/{id}/edit", name="edit_comment")
      */
-    public function editComment()
+    public function editComment(Comment $comment = null /*, Request $request,  ?UserInterface $user */)
     {
-        // A FAIRE !!!
-        return $this->render('comment/comment/index.html.twig', [
-            'controller_name' => 'CommentController',
-        ]);
+        if(!$comment) {
+            $comment = new Comment();
+        }
+        
+        // $form = $this->createForm(CommentType::class, $comment);
+
+        // $form->handleRequest($request);
+
+        // if ($form->isSubmitted() && $form->isValid()) {
+        //     // Si l'utilisateur est connecté
+        //     if($user) {
+        //         $comment->setUser($user);
+        //         $comment->setAnime($anime);
+
+        //         $entityManager = $this->getDoctrine()->getManager();
+        //         $entityManager->persist($comment);
+        //         $entityManager->flush();
+        //     }
+        // }
+
+        return $this->redirectToRoute("anime_show", [$anime->getSlug()]);
+        // return $this->render('comment/comment/index.html.twig', [
+        //     'controller_name' => 'CommentController',
+        // ]);
     }
 
     /**
      * supprimer
      * @Route("/{id}/delete", name="delete_comment")
      */
-    public function deleteComment(Comment $comment)
+    public function deleteComment(Comment $comment, ?UserInterface $user)
     {
-        return [];
+        $animeSlug = $comment->getAnime()->getSlug();
+
+        if($comment->isAuthorizedEdit($user)) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($comment);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('anime_show', ['slug' => $animeSlug]);
     }
 }
