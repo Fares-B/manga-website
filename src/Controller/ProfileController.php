db1d7f8127c1ba766e22ca77704dd25bda8501eb
Fares-B <fares.bourih@gmail.com>
2020-03-07 00:15:42 +0100

    adding a profile page

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+
+/**
+ * @Route("/profile")
+ */
+class ProfileController extends AbstractController
+{
+    /**
+     * @Route("/{username}", name="user_profile")
+     */
+    public function userProfile(User $user)
+    {
+        
+        return $this->render('profile/user_profile.html.twig', [
+            'user' => $user,
+        ]);
+    }
+}
